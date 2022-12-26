2b317926e734635dd741253d235c64a16397938b
monssif01 <121376944+monssif01@users.noreply.github.com>
2022-12-26 13:36:33 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/task14.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int n,t,i;
+    cout<<"give the number n";
+    cin>>n;
+    for(t=0;t<=n;t++){
+     for(i=0;i<=1;i++){
+             if(t>n)         
+                      
+             
+             
